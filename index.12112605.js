62414f8a7c73dc775ab4c3d8eb34aca9ade3f0c4
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-15 16:59:36 +0300

    make build

---
+++ index.12112605.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=e.getBoundingClientRect(),l=document.querySelector(".wall");e.style.top=l.clientHeight/2-t.height/2+"px",e.style.left=l.clientWidth/2-t.width/2+"px";
+//# sourceMappingURL=index.12112605.js.map
